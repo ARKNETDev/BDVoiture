2a345be77d014f6649e7bcfe84b7e168efe443ac
Antoine ANET <antoine.anet@laposte.net>
2016-10-12 23:29:09 +0200

    Add Insert

--- src/com/iia/btse2/Carsgbd/Main.java
+++ src/com/iia/btse2/Carsgbd/Main.java
@@ -1,14 +1,20 @@
 package com.iia.btse2.Carsgbd;
 
-import java.io.Console;
 import java.util.Scanner;
+
+import com.iia.btse2.Car.dao.VoitureDAO;
+import com.iia.btse2.Car.entity.Voiture;
 
 public class Main {
 
 	public static void main(String[] args) {
+		//Création de la voiture DAO
+		VoitureDAO cDao = new VoitureDAO();
+		boolean result;
 		Scanner scan = new Scanner(System.in);
 		int application = 1;
 		int choix = 0;
+		int i = 0;
 		System.out.println("Bienvenue dans notre Magasin !");
 		System.out.println("");
 		System.out.println("1 - RESTER ");
@@ -21,17 +27,47 @@ public class Main {
 				System.out.println("1 - Insérer une voiture");
 				System.out.println("2 - Modifier une voiture");
 				System.out.println("3 - Supprimer une voiture");
-				System.out.println("4 - Afficher toutes les voitures");
-				System.out.println("5 - Quitter");
+				System.out.println("4 - Afficher une voiture");
+				System.out.println("5 - Afficher toutes les voitures");
+				System.out.println("6 - Quitter");
 				System.out.println("");
 				choix = scan.nextInt();
 				switch (choix) {
 				case 1:
-					System.out.println("Insérer");
+					String marque, modele, couleur;
+					int annee, vitesse;
+					float prix;
+					Voiture newcar = new Voiture();
+					
+					System.out.println("Saissisez la marque");
+					marque = scan.nextLine();
+					newcar.setMarque(marque);
+					
+					System.out.println("Saissisez l'année");
+					annee = scan.nextInt();
+					newcar.setAnnee(annee);
+					
+					System.out.println("Saissisez le modele");
+					modele = scan.nextLine();
+					newcar.setModele(modele);
+					
+					System.out.println("Saissisez la couleur");
+					couleur = scan.nextLine();
+					newcar.setCouleur(couleur);
+					
+					System.out.println("Saissisez le prix");
+					prix = scan.nextFloat();
+					newcar.setPrix(prix);
+					
+					System.out.println("Saissisez le vitesse");
+					vitesse = scan.nextInt();
+					newcar.setVitesse(vitesse);
+					
+					result = cDao.create(newcar);
 					break;
 
 				case 2:
-					System.out.println("Modifier");
+					
 					break;
 					
 				case 3:
