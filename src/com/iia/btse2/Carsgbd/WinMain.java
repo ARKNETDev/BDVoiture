9e4fab284041aa6a633f46ddd3f84373bfe1f12a
Antoine ANET <antoine.anet@laposte.net>
2016-11-23 01:21:54 +0100

    Finish Project

--- src/com/iia/btse2/Carsgbd/WinMain.java
+++ src/com/iia/btse2/Carsgbd/WinMain.java
@@ -4,6 +4,8 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.ComboBoxModel;
 import javax.swing.JButton;
 import java.awt.GridLayout;
 import java.awt.GridBagLayout;
@@ -13,19 +15,33 @@ import java.awt.GridBagConstraints;
 import javax.swing.JLabel;
 import java.awt.Insets;
 import javax.swing.JTextField;
+
+import com.iia.btse2.Car.dao.Connexion;
 import com.iia.btse2.Car.dao.VoitureDAO;
 import com.iia.btse2.Car.entity.Voiture;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class WinMain {
+import javax.swing.JComboBox;
 
+public class WinMain extends JFrame {
+	VoitureDAO cDao = new VoitureDAO();
 	private JFrame frame;
-	private JTextField Txt_Identifiant;
 	private JTextField Txt_Marque;
 	private JTextField Txt_Annee;
 	private JTextField Txt_Couleur;
 	private JTextField Txt_Modele;
 	private JTextField Txt_Vitesse;
 	private JTextField Txt_Prix;
+	private JList list;
+	private JList list_1;
+	private JComboBox comboBox;
 
 	/**
 	 * Launch the application.
@@ -48,37 +64,95 @@ public class WinMain {
 	 */
 	public WinMain() {
 		initialize();
+
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		VoitureDAO cDao = new VoitureDAO();
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 629, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
-		
-		JButton btnInsertcar = new JButton("INSERTCAR");
+
+		JButton btnInsertcar = new JButton("INSERT CAR");
+		btnInsertcar.addActionListener(new ActionListener() {
+			Voiture insertcar = new Voiture();
+			VoitureDAO cDao = new VoitureDAO();
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == btnInsertcar) {
+
+					if (Txt_Marque == null) {
+						JOptionPane.showMessageDialog(null, "Champs manquants");
+					}
+					if (Txt_Modele.getText() == "") {
+						JOptionPane.showMessageDialog(null, "Champs manquants");
+					}
+					if (Txt_Couleur.getText() == "") {
+						JOptionPane.showMessageDialog(null, "Champs manquants");
+					}
+					if (Txt_Annee.getText() == "") {
+						JOptionPane.showMessageDialog(null, "Champs manquants");
+					}
+					if (Txt_Vitesse.getText() == "") {
+						JOptionPane.showMessageDialog(null, "Champs manquants");
+					}
+					if (Txt_Prix.getText() == "") {
+						JOptionPane.showMessageDialog(null, "Champs manquants");
+					} else {
+						insertcar.setMarque(WinMain.this.Txt_Marque.getText());
+						insertcar.setModele(WinMain.this.Txt_Modele.getText());
+						insertcar.setCouleur(WinMain.this.Txt_Couleur.getText());
+						insertcar.setAnnee(WinMain.this.Txt_Annee.getText());
+						insertcar.setVitesse(Integer.parseInt(WinMain.this.Txt_Vitesse.getText()));
+						insertcar.setPrix(Integer.parseInt(WinMain.this.Txt_Prix.getText()));
+						cDao.create(insertcar);
+						cDao.findAll().toArray();
+						JOptionPane.showMessageDialog(null, "Element inséré");
+
+					}
+
+				}
+			}
+		});
+
 		menuBar.add(btnInsertcar);
-		
+
 		JButton btnUpdateCar = new JButton("UPDATE CAR");
+		btnUpdateCar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		menuBar.add(btnUpdateCar);
-		
+
 		JButton btnDeleteCar = new JButton("DELETE CAR");
+		btnDeleteCar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (e.getSource() == btnDeleteCar) {
+					deletecar(comboBox.getSelectedItem().toString());
+					JOptionPane.showMessageDialog(null, "Element supprimé");
+				}
+			}
+
+		});
 		menuBar.add(btnDeleteCar);
-		
+
 		JButton btnExit = new JButton("EXIT");
 		menuBar.add(btnExit);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+				Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 1.0, Double.MIN_VALUE };
 		frame.getContentPane().setLayout(gridBagLayout);
-		
+
 		JLabel lblIdentifiant = new JLabel("Identifiant");
 		GridBagConstraints gbc_lblIdentifiant = new GridBagConstraints();
 		gbc_lblIdentifiant.anchor = GridBagConstraints.EAST;
@@ -86,16 +160,33 @@ public class WinMain {
 		gbc_lblIdentifiant.gridx = 0;
 		gbc_lblIdentifiant.gridy = 0;
 		frame.getContentPane().add(lblIdentifiant, gbc_lblIdentifiant);
-		
-		Txt_Identifiant = new JTextField();
-		GridBagConstraints gbc_Txt_Identifiant = new GridBagConstraints();
-		gbc_Txt_Identifiant.insets = new Insets(0, 0, 5, 5);
-		gbc_Txt_Identifiant.fill = GridBagConstraints.HORIZONTAL;
-		gbc_Txt_Identifiant.gridx = 1;
-		gbc_Txt_Identifiant.gridy = 0;
-		frame.getContentPane().add(Txt_Identifiant, gbc_Txt_Identifiant);
-		Txt_Identifiant.setColumns(10);
-		
+
+		comboBox = new JComboBox();
+		comboBox.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String id = comboBox.getSelectedItem().toString();
+				String req = String.format("Select * FROM Car WHERE Identifiant = " + id + ";");
+				try {
+					PreparedStatement st = Connexion.getConnection().prepareStatement(req);
+					st.setString(2, (String) comboBox.getSelectedItem());
+
+					ResultSet rs = st.executeQuery(req);
+					while (rs.next()) {
+						Txt_Marque.setText("Marque");
+					}
+				} catch (SQLException ex) {
+					System.out.println("erreur lors de la récupération des champs de la voiture");
+				}
+			}
+		});
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 0;
+		frame.getContentPane().add(comboBox, gbc_comboBox);
+
 		JLabel lblAnnee = new JLabel("Annee");
 		GridBagConstraints gbc_lblAnnee = new GridBagConstraints();
 		gbc_lblAnnee.anchor = GridBagConstraints.EAST;
@@ -103,7 +194,7 @@ public class WinMain {
 		gbc_lblAnnee.gridx = 2;
 		gbc_lblAnnee.gridy = 0;
 		frame.getContentPane().add(lblAnnee, gbc_lblAnnee);
-		
+
 		Txt_Annee = new JTextField();
 		GridBagConstraints gbc_Txt_Annee = new GridBagConstraints();
 		gbc_Txt_Annee.insets = new Insets(0, 0, 5, 5);
@@ -112,7 +203,7 @@ public class WinMain {
 		gbc_Txt_Annee.gridy = 0;
 		frame.getContentPane().add(Txt_Annee, gbc_Txt_Annee);
 		Txt_Annee.setColumns(10);
-		
+
 		JLabel lblModele = new JLabel("Modele");
 		GridBagConstraints gbc_lblModele = new GridBagConstraints();
 		gbc_lblModele.anchor = GridBagConstraints.EAST;
@@ -120,7 +211,7 @@ public class WinMain {
 		gbc_lblModele.gridx = 4;
 		gbc_lblModele.gridy = 0;
 		frame.getContentPane().add(lblModele, gbc_lblModele);
-		
+
 		Txt_Modele = new JTextField();
 		GridBagConstraints gbc_Txt_Modele = new GridBagConstraints();
 		gbc_Txt_Modele.anchor = GridBagConstraints.NORTH;
@@ -130,7 +221,7 @@ public class WinMain {
 		gbc_Txt_Modele.gridy = 0;
 		frame.getContentPane().add(Txt_Modele, gbc_Txt_Modele);
 		Txt_Modele.setColumns(10);
-		
+
 		JLabel lblPrix = new JLabel("Prix");
 		GridBagConstraints gbc_lblPrix = new GridBagConstraints();
 		gbc_lblPrix.anchor = GridBagConstraints.EAST;
@@ -138,7 +229,7 @@ public class WinMain {
 		gbc_lblPrix.gridx = 6;
 		gbc_lblPrix.gridy = 0;
 		frame.getContentPane().add(lblPrix, gbc_lblPrix);
-		
+
 		Txt_Prix = new JTextField();
 		GridBagConstraints gbc_Txt_Prix = new GridBagConstraints();
 		gbc_Txt_Prix.insets = new Insets(0, 0, 5, 5);
@@ -147,7 +238,7 @@ public class WinMain {
 		gbc_Txt_Prix.gridy = 0;
 		frame.getContentPane().add(Txt_Prix, gbc_Txt_Prix);
 		Txt_Prix.setColumns(10);
-		
+
 		JLabel lblMarque = new JLabel("Marque");
 		GridBagConstraints gbc_lblMarque = new GridBagConstraints();
 		gbc_lblMarque.anchor = GridBagConstraints.EAST;
@@ -155,7 +246,7 @@ public class WinMain {
 		gbc_lblMarque.gridx = 0;
 		gbc_lblMarque.gridy = 1;
 		frame.getContentPane().add(lblMarque, gbc_lblMarque);
-		
+
 		Txt_Marque = new JTextField();
 		GridBagConstraints gbc_Txt_Marque = new GridBagConstraints();
 		gbc_Txt_Marque.insets = new Insets(0, 0, 5, 5);
@@ -164,7 +255,7 @@ public class WinMain {
 		gbc_Txt_Marque.gridy = 1;
 		frame.getContentPane().add(Txt_Marque, gbc_Txt_Marque);
 		Txt_Marque.setColumns(10);
-		
+
 		JLabel lblCouleur = new JLabel("Couleur");
 		GridBagConstraints gbc_lblCouleur = new GridBagConstraints();
 		gbc_lblCouleur.anchor = GridBagConstraints.EAST;
@@ -172,7 +263,7 @@ public class WinMain {
 		gbc_lblCouleur.gridx = 2;
 		gbc_lblCouleur.gridy = 1;
 		frame.getContentPane().add(lblCouleur, gbc_lblCouleur);
-		
+
 		Txt_Couleur = new JTextField();
 		GridBagConstraints gbc_Txt_Couleur = new GridBagConstraints();
 		gbc_Txt_Couleur.insets = new Insets(0, 0, 5, 5);
@@ -181,7 +272,7 @@ public class WinMain {
 		gbc_Txt_Couleur.gridy = 1;
 		frame.getContentPane().add(Txt_Couleur, gbc_Txt_Couleur);
 		Txt_Couleur.setColumns(10);
-		
+
 		JLabel lblVitesse = new JLabel("Vitesse");
 		GridBagConstraints gbc_lblVitesse = new GridBagConstraints();
 		gbc_lblVitesse.anchor = GridBagConstraints.EAST;
@@ -189,7 +280,7 @@ public class WinMain {
 		gbc_lblVitesse.gridx = 4;
 		gbc_lblVitesse.gridy = 1;
 		frame.getContentPane().add(lblVitesse, gbc_lblVitesse);
-		
+
 		Txt_Vitesse = new JTextField();
 		GridBagConstraints gbc_Txt_Vitesse = new GridBagConstraints();
 		gbc_Txt_Vitesse.insets = new Insets(0, 0, 5, 5);
@@ -198,14 +289,64 @@ public class WinMain {
 		gbc_Txt_Vitesse.gridy = 1;
 		frame.getContentPane().add(Txt_Vitesse, gbc_Txt_Vitesse);
 		Txt_Vitesse.setColumns(10);
-		
-		JList list = new JList();
+
+		list_1 = new JList(cDao.findAll().toArray());
 		GridBagConstraints gbc_list = new GridBagConstraints();
 		gbc_list.gridwidth = 11;
 		gbc_list.fill = GridBagConstraints.BOTH;
 		gbc_list.gridx = 0;
 		gbc_list.gridy = 2;
-		frame.getContentPane().add(list, gbc_list);
+		frame.getContentPane().add(list_1, gbc_list);
+
+		displayID();
+		cDao.findAll().toArray();
 	}
 
+	public void displayID() {
+		String req = "SELECT Identifiant FROM Car;";
+		try {
+			Statement st = Connexion.getConnection().createStatement();
+			ResultSet rs = st.executeQuery(req);
+
+			while (rs.next()) {
+				comboBox.addItem(rs.getString("Identifiant"));
+			}
+		} catch (SQLException e) {
+			System.out.println("erreur lors de la récupération de l'ID de la voiture");
+		}
+		return;
+	}
+
+	public void deletecar(String id) {
+		String req = String.format("DELETE FROM Car WHERE Identifiant=" + id + ";");
+
+		try {
+			Statement st = Connexion.getConnection().createStatement();
+			if (st.executeUpdate(req) == 1) {
+				return;
+			}
+		} catch (SQLException e) {
+			System.out.println("erreur lors de la suppression de la voiture");
+		}
+		return;
+	}
+
+	public void displayChamps(String id) {
+		String req = String.format("Select Marque FROM Car WHERE Identifiant=" + id + ";");
+		try {
+			Statement st = Connexion.getConnection().createStatement();
+			ResultSet rs = st.executeQuery(req);
+			if (st.executeUpdate(req) == 1) {
+				while (rs.next()) {
+					String Marque = "dedz";
+					Marque = Txt_Marque.getText();
+				}
+				return;
+			}
+
+		} catch (SQLException e) {
+			System.out.println("erreur lors de la récupération de l'ID de la voiture");
+		}
+		return;
+	}
 }
